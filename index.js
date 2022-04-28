74615337136173dc663c4ef8d0983403e8f74ca8
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2022-04-27 23:26:46 -0300

    Adição de novas funcionalidades

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+
+app.use(express.static('public'))
+
+//Informando ao EXPRESS a engine que estou utilizando
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('screenPassword.ejs')
+})
+app.get('/home', (req, res) => {
+    res.render('home.ejs')
+})
+
+//Iniciando aplicação
+app.listen(3000, () => {
+    console.log('Aplicação iniciada 🚀 ')
+})
